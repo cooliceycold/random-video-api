891c1b7aede5ecd912768e48a49487f1eeec0dd9
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2024-07-30 17:00:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video Player</title>
     <!-- 引入 DPlayer 的 CSS 文件 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.css">
+    <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/dplayer/dist/DPlayer.min.css">
 </head>
 <body>
     <!-- 视频播放器容器 -->
@@ -21,7 +21,7 @@
     ?>
 
     <!-- 引入 DPlayer 的 JS 文件 -->
-    <script src="https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js"></script>
+    <script src="https://fastly.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js"></script>
     <script>
         const dp = new DPlayer({
             container: document.getElementById('dplayer'),
